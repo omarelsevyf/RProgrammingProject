2d1602214efa7241065fa9795c3eafece6f9fc56
Omar Elsevyf <79386058+omarelsevyf@users.noreply.github.com>
2021-03-04 14:54:09 -0400

    Delete ProyectoFinal.R

--- ProyectoFinal.R
+++
@@ -1,6 +0,0 @@
-##Final Project
-
-outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character") #se lee colclasses como character para que los id con leading zero no elimine los ceros por delante
-
-outcome[, 11] <- as.numeric(outcome[, 11]) #convierte de caracter a numerico.
-hist(outcome[, 11])
